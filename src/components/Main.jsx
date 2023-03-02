b9f5662c4c1582872e801015c9b3642e72fe4a32
Nathan <nathan.joshua95@gmail.com>
2023-03-02 20:24:49 +0700

    fix main bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,31 +9,22 @@ import { Trailer } from './Trailer';
 
 export const Main = (props) => {
 
+    // UI status
+    const [moviePlay, setMoviePlay] = useState(false);
+    const [movie, setOneMovies] = useState();
+    const [isLoading, setLoading] = useState(true);
+    const [animation, startAnimation] = useState(false);
+    const [isSoundOn, setSound] = useState(true);
+
     //modal context
     const { showModal } = useContext(ModalContext);
     const LogoImage = props.logo;
-    const videoUrl = props.video;
     const movieId = props.id;
     const type = props.type;
+    const text = '';
     let api = props.api;
 
-    // UI status
-    const [moviePlay, setMoviePlay] = useState(false);
-    const [movie, setOneMovies] = useState([]);
-    const [isLoading, setLoading] = useState(true);
-    const [animation, startAnimation] = useState(false);
-    const [onMovieHover, setHover] = useState(0);
-    const [isSoundOn, setSound] = useState(true);
-
-    const setMovieId = () => {
-        api =  api.replace('movieId', movieId);
-    }
-
-    const getData = async () => {
-        await axios.get(api).then((response)=>{
-            setOneMovies(response.data);
-        });
-    };
+    
     useEffect(() =>{
         const handleMoviePlay = () => {
             setTimeout(()=>{
@@ -52,47 +43,47 @@ export const Main = (props) => {
                 startAnimation(false)
             },23000)
         }
+       
+        const setMovieId = () => {
+            api =  api.replace('movieId', movieId);
+            
+        }
+        const getData = async () => {
+            await axios.get(api).then((response)=>{
+                setOneMovies(response.data);
+                
+                setLoading(false);
+            });
+        };
         setMovieId();
         getData();
         
-        setLoading(false);
-        
         handleMoviePlay();
+        
     },[api]);
+
+    const limitText = (str, num) => {
+            if(str.length > num){
+                return str.slice(0, num) + '...';
+            }
+            else return str;
+        }
 
     const SoundHandler = () => {
         setSound(!isSoundOn);
     }
 
-    const onHoverHandler=()=>{
-        setTimeout(
-            () => setHover(1), 
-            500
-          );
-    }
-    const onLeaveHandler=()=>{
-        setTimeout(
-            () => setHover(0), 
-            500
-          );
-    }
-
-    const limitText = (str, num) => {
-        if(str.length > num){
-            return str.slice(0, num) + '...';
-        }
-        else return str;
-    }
+    
 
     if (isLoading) {
         return <div className="App">Loading...</div>;
     }
     return (
-        <div className='relative w-full h-[800px] text-white' onMouseEnter={onHoverHandler} onMouseLeave={onLeaveHandler}>
+        <div className='relative w-full h-[800px] text-white'>
             <div className='w-full h-full'>
                 <div className='absolute w-full bg-gradient-to-t from-black  h-[800px] pl-[50px] flex items-end pb-[300px] justify-between'>
                     <div className='flex flex-col align-baseline justify-start'>
-                        {onMovieHover === 1 ? 
+                        {animation ? 
                         <div className='text-white text-start opacity-0 lg:w-[40%] md:w-[60%] xs:w-[90%] mb-5'>
                             {limitText(movie.overview,200)}
                         </div>
@@ -105,7 +96,7 @@ export const Main = (props) => {
                             <img  className='w-[500px]' src={process.env.PUBLIC_URL + LogoImage} alt={movie.title}/>
                         </div>
                         
-                        {onMovieHover === 0 ? 
+                        {!animation ? 
                         <div className='text-white text-start lg:w-[40%] md:w-[60%] xs:w-[90%] mb-5 duration-1000'>
                             {limitText(movie.overview,200)}
                         </div>
